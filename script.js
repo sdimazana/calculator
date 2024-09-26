6aa01c271f7de7f24080c18d65ad0cd7a110bde6
sdimazana <sdimazana@gmail.com>
2024-09-25 18:45:05 -0700

    Add some calculator functionality and UI
    This commit includes most of the logic for
    the calculator to function correctly. However,
    logic inside the operationButtons.forEach block
    needs to be edited such that it does not store
    equal signs as "operations".

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let firstOperand;
 let secondOperand;
 let operation;
+let previousOperation;
+let result;
 
 function add(x, y){
     return x + y;
@@ -21,26 +23,129 @@ function divide(x, y){
 function operate(operator, firstOperand, secondOperand){
     switch(operator){
         case "+":
-            add(firstOperand, secondOperand);
-            break;
+            return add(firstOperand, secondOperand);
         case "-":
-            subtract(firstOperand, secondOperand);
-            break;
-        case "*":
-            multiply(firstOperand, secondOperand);
-            break;
-        case "/":
-            divide(firstOperand, secondOperand);
-            break;
+            return subtract(firstOperand, secondOperand);
+        case "×":
+            return multiply(firstOperand, secondOperand);
+        case "➗":
+            return divide(firstOperand, secondOperand);
     }
 }
 
+const equalButton = document.getElementById("equal-sign");
 const display = document.getElementById("display");
 const digitButtons = document.getElementById("digit-buttons").querySelectorAll("button");
+const operationButtons = document.getElementById("operations-buttons").querySelectorAll("button");
+const miscButtons = document.getElementById("misc-buttons").querySelectorAll("button");
+let decimalButton = document.getElementById("decimal");
+let clearDisplayFlag = false;
+let operandsArray = [];
+let operationArray = [];
 
 digitButtons.forEach((button) => {
     button.addEventListener("click", () => {
+        operationButtons.forEach((opButton) => {
+            opButton.removeAttribute("style");
+        });
+
+        if(clearDisplayFlag){
+            display.textContent = "";
+            clearDisplayFlag = false;
+        }
+    
+        if(display.textContent.includes(".")){
+            decimalButton.disabled = true;
+            decimalButton.style.filter = "brightness(0.75)";
+        }else{
+            decimalButton.disabled = false;
+            decimalButton.removeAttribute("style");
+        }
+
         display.textContent += button.textContent;
+
     });
 });
+
+operationButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        // Highlight the operation buttons in the UI when clicked
+        if(button.textContent !== "=") button.style.filter = "brightness(1.16)";
+        decimalButton.disabled = false;
+        decimalButton.removeAttribute("style");
+        clearDisplayFlag = true;
+
+        operandsArray.splice(operandsArray.length, 0, Number(display.textContent));
+        operationArray.splice(operationArray.length, 0, button.textContent);
+    
+        if(operandsArray.length >= 2 && operationArray.length >= 2){
+            firstOperand = operandsArray[operandsArray.length-2];
+            secondOperand = operandsArray[operandsArray.length-1]
+            operation = operationArray[operationArray.length-2]
+
+            if(secondOperand === 0 && operation === "➗"){
+                display.textContent = "JAIL";
+            }else{
+                result = operate(operation, firstOperand, secondOperand);
+            }
+            // if(result%1 !== 0) result = result.toFixed(2);
+            display.textContent = result; 
+            operandsArray.splice(operandsArray.length, 0, Number(result));
+        }
+        
+    });
+});
+
+miscButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        switch(button.innerText){
+            case "AC":
+                firstOperand = 0;
+                secondOperand = 0;
+                operation = "";
+                operandsArray = [];
+                operationArray = [];
+                display.textContent = "";
+                clearDisplayFlag = false;
+                equalSignFlag = false;
+                break;
+            case "+/-":
+                display.textContent = `-${display.textContent}`;
+                break;
+            case "%":
+                let currentValue = +display.textContent;
+                let percentage = currentValue/100;
+                display.textContent = `${percentage}`;
+                break;
+        }
+    });
+});
+
+document.addEventListener("keyup", (event) =>{
+    console.log(event.key);
+    if(event.key === "Backspace"){
+        let displayContent = display.textContent.split("");
+        displayContent.pop();
+        display.textContent = displayContent.join("");
+    }else if(event.key >= 0 && event.key <= 9){
+        display.textContent += event.key;
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
