e5d653f4c76792c79b488308c7af5142c87b444e
sdimazana <sdimazana@gmail.com>
2024-09-25 20:51:16 -0700

    Complete project

--- script.js
+++ script.js
@@ -67,35 +67,6 @@ digitButtons.forEach((button) => {
     });
 });
 
-// operationButtons.forEach((button) => {
-//     button.addEventListener("click", () => {
-//         // Highlight the operation buttons in the UI when clicked
-//         if(button.textContent !== "=") button.style.filter = "brightness(1.16)";
-//         decimalButton.disabled = false;
-//         decimalButton.removeAttribute("style");
-//         clearDisplayFlag = true;
-
-//         operandsArray.splice(operandsArray.length, 0, Number(display.textContent));
-//         operationArray.splice(operationArray.length, 0, button.textContent);
-    
-//         if(operandsArray.length >= 2 && operationArray.length >= 2){
-//             firstOperand = operandsArray[operandsArray.length-2];
-//             secondOperand = operandsArray[operandsArray.length-1]
-//             operation = operationArray[operationArray.length-2]
-
-//             if(secondOperand === 0 && operation === "➗"){
-//                 display.textContent = "JAIL";
-//             }else{
-//                 result = operate(operation, firstOperand, secondOperand);
-//             }
-//             // if(result%1 !== 0) result = result.toFixed(2);
-//             display.textContent = result; 
-//             operandsArray.splice(operandsArray.length, 0, Number(result));
-//         }
-        
-//     });
-// });
-
 operationButtons.forEach((button) => {
     button.addEventListener("click", () => {
         // Highlight the operation buttons in the UI when clicked
@@ -166,27 +137,87 @@ miscButtons.forEach((button) => {
     });
 });
 
+const additionButton = document.getElementById("plus-sign");
+const subtractionButton = document.getElementById("sub-sign");
+const multiplicationButton = document.getElementById("mult-sign");
+const divisionButton = document.getElementById("div-sign");
+
 document.addEventListener("keyup", (event) =>{
     console.log(event.key);
     if(event.key === "Backspace"){
         let displayContent = display.textContent.split("");
         displayContent.pop();
         display.textContent = displayContent.join("");
-    }else if(event.key >= 0 && event.key <= 9){
+    }else if(event.key >= 0 && event.key <= 9 || event.key === "."){
+        operationButtons.forEach((button) => {
+            button.removeAttribute("style");
+        });
+
+        if(clearDisplayFlag){
+            display.textContent = "";
+            clearDisplayFlag = false;
+        }
+
+        if(display.textContent.includes(".")){
+            decimalButton.disabled = true;
+            decimalButton.style.filter = "brightness(0.75)";
+            if(event.key === "."){
+                return;
+            }
+        }else{
+            decimalButton.disabled = false;
+            decimalButton.removeAttribute("style");
+        }
+
         display.textContent += event.key;
-    }else if(event.key === "+" || event.key === "-" || event.key === "*" || event.key === "/" || event.key === "="){
+
+    }else if(event.key === "+" || event.key === "-" || event.key === "*" 
+        || event.key === "/" || event.key === "=" || event.key === "Enter"){
         // Highlight the operation buttons in the UI when clicked
-        if(event.key !== "=") button.style.filter = "brightness(1.16)";
+        if(event.key === "+") additionButton.style.filter = "brightness(1.16)";
+        if(event.key === "-") subtractionButton.style.filter = "brightness(1.16)";
+        if(event.key === "*") multiplicationButton.style.filter = "brightness(1.16)";
+        if(event.key === "/") divisionButton.style.filter = "brightness(1.16)";
         decimalButton.disabled = false;
         decimalButton.removeAttribute("style");
         clearDisplayFlag = true;
 
+        operandsArray.splice(operandsArray.length, 0, Number(display.textContent));
         
+        // As long as not equal sign, add operation symbols to the array
+        if(event.key !== "=" || event.key !== "Enter"){
+            operationArray.splice(operationArray.length, 0, event.key);
+
+            // If there's only one element in the array, don't use operationArray.length-2
+            if(operationArray.length < 2){
+                operation = operationArray[operationArray.length-1];
+            }else{
+                operation = operationArray[operationArray.length-2];
+            }
+
+            // If there's only one element in the array, don't use operandArray.length-2
+            if(operandsArray.length < 2){
+                firstOperand = operandsArray[operandsArray.length-1];
+            }else{
+                firstOperand = operandsArray[operandsArray.length-2];
+                secondOperand = operandsArray[operandsArray.length-1];
+                result = operate(operation, firstOperand, secondOperand);
+                display.textContent = result;
+                operandsArray.splice(operandsArray.length, 0, Number(result));
+
+            }
+        }else{
+            firstOperand = operandsArray[operandsArray.length-2];
+            secondOperand = operandsArray[operandsArray.length-1];
+            operation = operationArray[operationArray.length-1];
+            result = operate(operation, firstOperand, secondOperand);
+            display.textContent = result;
+            operandsArray = [];
+            operationArray = [];
+        }
+
     }
 });
-
-
-
 
 
 
